3da7e5ec535e37e9977b6aace13a1efd66121e7d
pgatter <olofgatter@gmail.com>
2013-12-26 23:08:01 +0100

    Initial commit

---
+++ core/ConfigParser.py
@@ -0,0 +1,23 @@
+'''
+Created on Dec 26, 2013
+
+@author: olofgatter
+'''
+
+#
+# Parser that reads the configuration file
+# written by the user.
+#
+class Generator(object):
+    '''
+    Generates Python classes based on the mapping file.
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+    def generateClasses(self, mapperFile):
+        return
+        
